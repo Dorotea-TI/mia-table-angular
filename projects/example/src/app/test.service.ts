4fa7182841a01f8255b8a3de5ca9a5fe64fad2ac
HeiderHDev <heiderhrnndz@gmail.com>
2024-10-09 11:54:38 -0500

    feat: 🎸 doroteati dependencies are imported into the service

--- projects/example/src/app/test.service.ts
+++ projects/example/src/app/test.service.ts
@@ -1,17 +1,23 @@
-import { MiaBaseCrudHttpService, MiaPagination, MiaQuery } from '@agencycoda/mia-core';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import {
+  MIA_CORE_PROVIDER,
+  MiaBaseCrudHttpService,
+  MiaCoreConfig,
+  MiaPagination,
+  MiaQuery,
+} from '@doroteati/mia-core';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TestService extends MiaBaseCrudHttpService<any> {
-
   constructor(
-    protected http: HttpClient
-  ) { 
-    super(http);
+    protected http: HttpClient,
+    @Inject(MIA_CORE_PROVIDER) protected config: MiaCoreConfig
+  ) {
+    super(config, http);
     this.basePathUrl = 'https://ecotopia-dev.uc.r.appspot.com/mia-blog';
   }
 
@@ -21,7 +27,9 @@ export class TestService extends MiaBaseCrudHttpService<any> {
     return this.postOb(this.basePathUrl + '/list', params);
   }
 
-  list(query: MiaQuery): Promise<MiaPagination<any>> {
-    return this.listWithExtras(query, { access_token: 'aa9676c80bc803b902522459852365847dceb447'});
+  list(query: MiaQuery): Observable<MiaPagination<any>> {
+    return this.listWithExtras(query, {
+      access_token: 'aa9676c80bc803b902522459852365847dceb447',
+    });
   }
 }
