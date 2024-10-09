170530d90d2368ee9f571803ffbbbe1ec2367c09
HeiderHDev <heiderhrnndz@gmail.com>
2024-10-09 11:16:33 -0500

    feat: 🎸 Renamed from agencycoda to doroteati

--- projects/doroteati/mia-table/karma.conf.js
+++ projects/doroteati/mia-table/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../../coverage/agencycoda/mia-table'),
+      dir: require('path').join(__dirname, '../../../coverage/doroteati/mia-table'),
       subdir: '.',
       reporters: [
         { type: 'html' },
